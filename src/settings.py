98f2534546258589057d6ab711306c338676f130
hchkrdtn <za4rynsky@gmail.com>
2017-03-26 20:45:15 -0500

    Initial commit

---
+++ src/settings.py
@@ -0,0 +1,17 @@
+_author_ = 'hchkrdtn'
+
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left.
+self.fleet_direction = 1
