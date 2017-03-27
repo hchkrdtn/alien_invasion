f86b3dee45159c439c5a8acf553f8c2c39a90729
hchkrdtn <za4rynsky@gmail.com>
2017-03-26 21:01:41 -0500

    Ship movement and key responses.

--- src/game_functions.py
+++ src/game_functions.py
@@ -3,17 +3,39 @@ import pygame
 
 _author_ = 'hchkrdtn'
 
-def check_events():
-    """"Respond to keypresses and mouse events."""
+
+def check_keydown_events(event, ai_settings, screen, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ai_settings, screen, ship):
+    """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
-    """Update images on the screen and flip to the new screen"""
-    # Redraw the screen during each pass through the loop.
+    """Update images on the screen, and flip to the new screen."""
+    # Redraw the screen, each pass through the loop.
     screen.fill(ai_settings.bg_color)
+
     ship.blitme()
 
     # Make the most recently drawn screen visible.
-pygame.display.flip()
+    pygame.display.flip()
