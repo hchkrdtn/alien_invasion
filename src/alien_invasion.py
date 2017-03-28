ba1d31f083d97de2aa37a1a49253c1370b96e1bb
hchkrdtn <za4rynsky@gmail.com>
2017-03-28 00:05:19 -0500

    Add Play bottom and its logic, refactor settings

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -4,6 +4,7 @@ _author_ = 'hchkrdtn'
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from pygame.sprite import Group
 import game_functions as gf
@@ -14,6 +15,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -28,13 +32,13 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
